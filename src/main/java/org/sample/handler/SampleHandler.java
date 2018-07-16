034100fe07887a0fb2bb838dc4f5ab803884300e
Sören Sprößig <ssproessig@googlemail.com>
2018-07-16 17:11:49 +0200

    adding "simple logging façade for java" (works w/o POM change, why?!)

--- src/main/java/org/sample/handler/SampleHandler.java
+++ src/main/java/org/sample/handler/SampleHandler.java
@@ -4,12 +4,18 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class SampleHandler extends TextWebSocketHandler {
 
+  private static final Logger logger = LoggerFactory.getLogger(SampleHandler.class);
+
+
   @Override
   public void handleTextMessage(WebSocketSession session, TextMessage message) {
-
+    logger.debug(message.toString());
   }
 
 }
