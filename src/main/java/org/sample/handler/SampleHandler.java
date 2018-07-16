1f8aeb350d808e5fb53629cc005d22f65761364e
Sören Sprößig <ssproessig@googlemail.com>
2018-07-16 17:07:18 +0200

    adding low-level WebSocket handler at "/ws"

---
+++ src/main/java/org/sample/handler/SampleHandler.java
@@ -0,0 +1,15 @@
+package org.sample.handler;
+
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+
+public class SampleHandler extends TextWebSocketHandler {
+
+  @Override
+  public void handleTextMessage(WebSocketSession session, TextMessage message) {
+
+  }
+
+}
